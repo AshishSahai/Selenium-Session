b57b9391c09049b54e04ea49bee019dd75083ee3
Ashish Kumar <ashish0981kumar@gmail.com>
2022-05-25 17:42:36 +0530

    Automating Sign Up page

--- SignUp.java
+++ SignUp.java
@@ -10,16 +10,30 @@ public class SpotifySignUp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "/home/ashish/Downloads/ChromeDriver/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.spotify.com/us/signup");
+		WebDriver driver = new ChromeDriver();   // Launch Browser
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://www.spotify.com/us/signup");  // Sign Up Page
+		
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		
+		//Automation
 		driver.findElement(By.id("email")).sendKeys("ashish07kumarsss@gmail.com");
 		driver.findElement(By.id("confirm")).sendKeys("ashish07kumarsss@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("Jopy&123456");
 		driver.findElement(By.id("displayname")).sendKeys("Choo");
+		
+		// Handle Drop Down
 		Select select = new Select(driver.findElement(By.id("month")));
 		select.selectByVisibleText("June");
 		driver.findElement(By.id("day")).sendKeys("12");
 		driver.findElement(By.id("year")).sendKeys("1997");
+		
+		
 		driver.findElement(By.xpath("//*[@id=\"__next\"]/main/div/div/form/fieldset/div/div[1]/label/span[2]")).click();
 		driver.findElement(By.cssSelector("#__next > main > div > div > form > div:nth-child(8) > div > label > span.Indicator-sc-1airx73-0.ihUlHW")).click();
 		
